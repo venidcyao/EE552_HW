9457c4bc988d6e0484eea360cc2d98bc5b5afe21
venidcYao <venidcyao@gmail.com>
2017-11-16 22:50:26 -0500

    WeiQi

---
+++ WeiQi/src/Piece.java
@@ -0,0 +1,32 @@
+import java.awt.*;
+
+public class Piece {
+    private int x, y;
+    private String color;
+
+    Piece(int x, int y, String prevColor) {
+        this.x = x;
+        this.y = y;
+        if (prevColor.equals("black")) {
+            this.color = "white";
+        } else {
+            this.color = "black";
+        }
+    }
+
+    void draw(Graphics g) {
+        if (color.equals("black")) {
+            g.setColor(Color.black);
+        } else {
+            g.setColor(Color.white);
+        }
+
+        g.fillOval(x- 12, y- 12, 24, 24);
+    }
+    
+    public String getColor() {
+        return this.color;
+    }
+    
+
+}
